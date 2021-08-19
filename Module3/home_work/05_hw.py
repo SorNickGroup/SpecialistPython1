61aa929a1148d09b05a7c47930e5a0c87f216713
SorNickGroup <89096319+SorNickGroup@users.noreply.github.com>
2021-08-19 15:01:05 +0300

    Update 05_hw.py

--- Module3/home_work/05_hw.py
+++ Module3/home_work/05_hw.py
@@ -3,4 +3,13 @@
 
 names = ["Иван", "Ирина", "Вячеслав", "Василий", "Петр"]
 
-# TODO: your code here
+i=0
+max_len_word=0
+pos_longest_word=-1;
+while i<len(names):
+    current_len_word=len(names[i])
+    if(current_len_word>max_len_word):
+        pos_longest_word=i
+        max_len_word=current_len_word
+    i+=1
+print(names[pos_longest_word])
