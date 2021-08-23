d9f32c66d23af751bc6846e60f8c95ec7704d4d2
SorNickGroup <89096319+SorNickGroup@users.noreply.github.com>
2021-08-23 20:02:24 +0300

    Update 05_task_repeat.py

--- Module5/practice/repeat/05_task_repeat.py
+++ Module5/practice/repeat/05_task_repeat.py
@@ -1,3 +1,12 @@
 # Дано общее количество объектов для отображения на веб-сайте и количество объектов на каждой странице.
 # Вычислите, сколько объектов будет отображаться на последней странице сайта.
 
+def get_count_items_on_end_page(num_items, items_on_page):
+    count = num_items % items_on_page
+    if count == 0:
+        count += items_on_page
+    return count
+
+
+print(get_count_items_on_end_page(2, 3))
+print(get_count_items_on_end_page(21, 3))
