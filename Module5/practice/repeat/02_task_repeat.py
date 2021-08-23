f3312c4017e94e7fcfa59f9499d3026ebd304d02
SorNickGroup <89096319+SorNickGroup@users.noreply.github.com>
2021-08-23 19:28:55 +0300

    Update 02_task_repeat.py

--- Module5/practice/repeat/02_task_repeat.py
+++ Module5/practice/repeat/02_task_repeat.py
@@ -4,4 +4,10 @@
 #  Пример палиндрома: 12321
 
 def palindrome(number):
-    pass
+    inverted_number = 0
+    number_copy = number
+    while number_copy > 0:
+        last_digit = number_copy % 10
+        inverted_number = inverted_number * 10 + last_digit
+        number_copy = number_copy // 10
+    return inverted_number == number
