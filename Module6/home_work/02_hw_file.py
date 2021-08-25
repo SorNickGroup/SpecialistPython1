4ccf15908f6b7e31ac5ea80ca864c79aafc8c4d0
SorNickGroup <89096319+SorNickGroup@users.noreply.github.com>
2021-08-25 12:35:47 +0300

    Update 02_hw_file.py

--- Module6/home_work/02_hw_file.py
+++ Module6/home_work/02_hw_file.py
@@ -2,4 +2,11 @@
 # Найдите сумму всех чисел, пропуская все строки содержащие не числовые значения
 
 with open("data/info.txt", "r") as f:
-    pass
+    summa=0
+    for line in f:
+        line=line.rstrip()
+        if (line[0]=="-" or line[0]=="+") and line[1:len(line)].isdigit():
+            summa+=int(line)
+        elif line.isdigit():
+            summa+=int(line)
+    print(summa)
