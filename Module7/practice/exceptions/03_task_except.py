2cb16cac90f2543a15620652a16b79e0a200782b
SorNickGroup <89096319+SorNickGroup@users.noreply.github.com>
2021-08-26 08:10:01 +0300

    Update 03_task_except.py

--- Module7/practice/exceptions/03_task_except.py
+++ Module7/practice/exceptions/03_task_except.py
@@ -5,3 +5,19 @@
 
 # При решении задачи требуется учесть формат входных данных.
 # Если входные данные некорректные, сообщить об этом.
+while True:
+    input_str = input("Введите пять целых чисел, разделенных пробелом: ")
+    try:
+        numbers=input_str.split()
+        if len(numbers) != 5:
+            raise ValueError
+        min_number=int(numbers[0])
+        for number in numbers:
+            if min_number>int(number):
+                min_number=int(number)
+        print(min_number)
+        break
+    except ValueError:
+        print("Данные введены в неверно!")
+    except Exception as e:
+        print(e)
