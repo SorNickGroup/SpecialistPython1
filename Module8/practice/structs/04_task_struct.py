a17654faa5762b34a2c16ffd73a59239c1919264
SorNickGroup <89096319+SorNickGroup@users.noreply.github.com>
2021-08-26 20:42:02 +0300

    Update 04_task_struct.py

--- Module8/practice/structs/04_task_struct.py
+++ Module8/practice/structs/04_task_struct.py
@@ -2,3 +2,7 @@
 dictionary_1 = {'a': 300, 'b': 400}
 dictionary_2 = {'c': 500, 'd': 600}
 # Объедините их в один
+union_dict = dictionary_1.copy()
+union_dict.update(dictionary_2)
+
+print(union_dict)
