09173ba479af511dd12b56257997c85f8051dca6
SorNickGroup <89096319+SorNickGroup@users.noreply.github.com>
2021-08-26 20:20:00 +0300

    Update 03_task_struct.py

--- Module8/practice/structs/03_task_struct.py
+++ Module8/practice/structs/03_task_struct.py
@@ -4,3 +4,16 @@
 # 1. Количество элементов списка не превышающие 10
 # 2. Сумму всех положительных элементов списка
 # 3. Среднее арифметическое всех четных элементов
+import random
+
+numbers = [random.randint(-10, 10) for _ in range(10)]
+print(numbers)
+
+nums_less10 = [el for el in numbers if el < 10]
+print(len(nums_less10))
+
+nums_positive = [el for el in numbers if el > 0]
+print(sum(nums_positive))
+
+even_elements = [el for el in numbers if el % 2 == 0]
+print(sum(even_elements) / len(even_elements))
