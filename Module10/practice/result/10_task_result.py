2bb74f5580c5a00572cc36a3caace7c12ab572f4
SorNickGroup <89096319+SorNickGroup@users.noreply.github.com>
2021-08-28 02:52:15 +0300

    Update 10_task_result.py

--- Module10/practice/result/10_task_result.py
+++ Module10/practice/result/10_task_result.py
@@ -3,3 +3,21 @@
 # Примечание: все элементы списка должны быть гарантировано уникальными(неповторяющимися)
 # Если создать список с заданными параметрами невозможно - функция должна выбросить исключение(любое)
 
+import random
+
+def generate_uniq_random_list(size,min_val=-10,max_val=10):    
+    list_numbers = []
+    try:
+        if size>(max_val-min_val):
+            raise
+    except Exception:
+        print("Создать список с заданными параметрами невозможно!")
+    while(len(list_numbers)<size):
+        new_value=random.randint(min_val, max_val)
+        if list_numbers.count(new_value)==0:
+            list_numbers.append(new_value)
+    return list_numbers
+
+
+print(generate_uniq_random_list(10))
+print(generate_uniq_random_list(10,2,10))
