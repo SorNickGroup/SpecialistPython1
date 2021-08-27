53030d1d8102f1f78f0c7dfd7638e18154ecd6d3
SorNickGroup <89096319+SorNickGroup@users.noreply.github.com>
2021-08-27 19:42:48 +0300

    Update 01_task_regexp.py

--- Module9/practice/regexp/01_task_regexp.py
+++ Module9/practice/regexp/01_task_regexp.py
@@ -6,4 +6,19 @@
 # 4) все слова начинающиеся на согласную букву
 # 5) все уникальные(без дубликатов) знаки препинания
 
-text = ...
+import re
+
+text = "Why do we use it? It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)."
+
+template = r"^[A-Z]\w+"
+res = re.findall(template, text)
+print(res)
+template = r"\b\w{2}"
+res = re.findall(template, text)
+print(res)
+template = r"\b[aeiouy]\w+"
+res = re.findall(template, text)
+print(res)
+template = r"([^\w\s'])\s"
+res = re.findall(template, text)
+print(set(res))
