f35eb81048d4ec9afa432c7c4e567f85ef6cd42e
SorNickGroup <89096319+SorNickGroup@users.noreply.github.com>
2021-08-27 23:13:34 +0300

    Update 02_task_result.py

--- Module10/practice/result/02_task_result.py
+++ Module10/practice/result/02_task_result.py
@@ -6,3 +6,24 @@
 # Формат выходных данных:
 # Выведите, во сколько раз отрезок AB больше, чем отрезок CD. Ответ введите с точностью до 6-ти знаков после запятой.
 
+while True:
+    input_str = input("Введите четыре целых числа в формате: A, B, C и D: ")
+    snumbers = input_str.split(",")
+    numbers = []
+    try:
+        if len(snumbers) != 4:
+            raise ValueError
+        numbers = list(map(lambda str: int(str.strip()), snumbers))
+        break
+    except ValueError:
+        print("Данные введены в неверно!")
+
+len_ab=abs(numbers[1]-numbers[0])
+len_cd=abs(numbers[3]-numbers[2])
+
+if(len_ab>len_cd):
+    diff =len_ab/len_cd
+    print(f"Отрезок АВ={len_ab} больше СD={len_cd} в {diff:.6f} раз(-а)")
+else:
+    diff =len_cd/len_ab
+    print(f"Отрезок АВ={len_ab} меньше СD={len_cd} в {diff:.6f} раз(-а)")
